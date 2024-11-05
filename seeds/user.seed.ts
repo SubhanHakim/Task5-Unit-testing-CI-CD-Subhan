629e3c61fd68c52f84c82b5cc1ac2b4aa617589b
Subhan Hakim <subhanhakim07@gmail.com>
2024-11-05 15:20:29 +0700

    add: github actio fixn

--- seeds/user.seed.ts
+++ seeds/user.seed.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { User, IUser } from '../models/auth.models';
 import databaseConfig from '../config/database';
 
