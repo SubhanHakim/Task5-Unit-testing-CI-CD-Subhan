5e4ffa7f403cb424aff524e948547e8c5b25318f
Subhan Hakim <subhanhakim07@gmail.com>
2024-11-05 15:18:32 +0700

    add: github actio fixn

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import {User, IUser} from '../models/auth.models';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 interface SignInRequest {
