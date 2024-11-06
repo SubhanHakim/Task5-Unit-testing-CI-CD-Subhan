c9af0df0ec740ceb65308e11ef323303faf9e74d
Subhan Hakim <subhanhakim07@gmail.com>
2024-11-06 10:54:39 +0700

    testing6

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,16 +1,26 @@
-import mongoose from 'mongoose';
-
-beforeAll(async () => {
-  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo-test:27017/testdb');
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
-});
-
-afterEach(async () => {
-  await Promise.all(
-    Object.values(mongoose.connection.collections)
-      .map(collection => collection.deleteMany({}))
-  );
-});
+import mongoose from "mongoose";
+const TEST_DB_URL = "mongodb+srv://sinproject251201:subhan321@cluster0.rngsv.mongodb.net/book-task4";
+export const setupTestDB = async () => {
+  try {
+    await mongoose.connect(TEST_DB_URL);
+    console.log("Connected to test database");
+  } catch (error) {
+    console.error("Error connecting to test database:", error);
+    process.exit(1);
+  }
+};
+export const teardownTestDB = async () => {
+  try {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    console.log("Database connection closed");
+  } catch (error) {
+    console.error("Error closing database connection:", error);
+  }
+};
+export const clearTestDB = async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+};
